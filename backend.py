0dc60214e5b1a6885756926c4f2117f37c383b94
Nathan Chou <chou162@purdue.edu>
2026-05-01 19:26:24 -0400

    Fixed issues in backend.py

--- backend.py
+++ backend.py
@@ -459,4 +459,5 @@ def explain_query():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
